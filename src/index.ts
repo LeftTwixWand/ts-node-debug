84cef05b54c939f5872e5a5829b6c39288336096
LeftTwixWand <0965142838gvs@gmail.com>
2024-11-10 19:29:55 +0100

    Add octokit debug profile

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
+import { App } from "octokit";
 import { ConsoleLogger } from "./ConsoleLogger";
-
+ 
 const logger = new ConsoleLogger();
 
-logger.log("Hello, world from imported class!");
-console.log("Hello, world!");
+const INSTALLATION_ID = 56473199;
+const app = new App({appId: 56473199, privateKey: "NiJ9.eyJpbnN0YWxsYXRpb25JZCI6IjU2NDczMTk5IiwiaXNzIjoidnN0cyIsImF1ZCI6InZzdHMiLCJuYmYiOi02MjEzNTU5MzEzOSwiZXhwIjotNjIxMzU1OTMxMzl9.IanmI-mqETyCHUB84Og8i14QMTni4WraMm8QX4ixSx8"});
+
+const octokit = await app.getInstallationOctokit(INSTALLATION_ID);
+
+const respone = await octokit.graphql(`
+    query {
+      viewer {
+        login
+      }
+    }
+    `);
+
+    console.log(respone);
